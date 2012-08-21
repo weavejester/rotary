7799c19024a97d83448758b4c421452949bf3017
James Reeves <jreeves@weavejester.com>
2012-08-21 10:59:07 +0100

    Updated AWS SDK version

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Amazon DynamoDB API"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/algo.generic "0.1.0"]
-                 [com.amazonaws/aws-java-sdk "1.3.6"]]
+                 [com.amazonaws/aws-java-sdk "1.3.14"]]
   :plugins [[codox "0.6.1"]])
